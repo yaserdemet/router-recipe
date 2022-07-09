b33aa5176c6a037b4ff1e37e8148bf1fcd0afb90
yaser <ddemety372@gmail.com>
2022-07-10 01:56:53 +0300

    approaching to end

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 
+import Styled from './AllStyledCom';
 import './App.css';
 import AppRouter from './router/AppRouter';
+// import  AllStyledCom  from './AllStyledCom';
 
 
 function App() {
   return (
   <div>
+ <Styled />
    <AppRouter />
+
   </div>
   );
 }
